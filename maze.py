eb1fea79e0ccabb7769d75e2a36d4d313c9c5750
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-06-14 11:01:34 +0300

    Fixed bug generated by unit test

--- maze.py
+++ maze.py
@@ -2,7 +2,6 @@ from cell import Cell
 import random
 import time
 
-
 class Maze:
     def __init__(
         self,
@@ -12,7 +11,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win=None,
     ):
         self._cells = []
         self._x1 = x1
